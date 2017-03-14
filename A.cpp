b2e7c0612c371359aca097ae00aea7bd34c7f33c
sam1995520 <hsu889999@gmail.com>
2017-03-14 12:43:54 +0800

    Update A.cpp

--- A.cpp
+++ A.cpp
@@ -1,8 +1,21 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+
 int main()
-  {
-  std::cout << "Hello ";
-  cout << "NC" << "KU!!" << endl;
-  return 0;
-  }
+{
+    int n;
+    scanf("%d",&n);
+    printf("%d\n",n);
+
+    if(n==1)
+        return 0;
+    else
+    {
+        if(n%2==1)
+            n=3*n+1;
+        else
+        n=n/2;
+    printf("%d\n",n);
+    }
+    return 0;
+}
